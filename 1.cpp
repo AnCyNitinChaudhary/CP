753567a0640526e2f51c35de99a3c20749d8e110
AnCyNitinChaudhary <nitinchaudhary7290@gmail.com>
2024-08-08 01:53:25 +0530

    tempcommit

--- 1.cpp
+++ 1.cpp
@@ -13,35 +13,7 @@ cin>>t;
 while(t--){
    int n;
    cin>>n;
-   int a[n];
-   for(int i=0;i<n;i++){
-    cin>>a[i];
-   }
-   int k=0;
-   vector<int>ans;
-   while(true){
-    int mini=INT_MAX;
-    int maxi=INT_MIN;
-    for(int i=0;i<n;i++){
-        mini=min(mini,a[i]);
-        maxi=max(maxi,a[i]);
-    }
-    int mid=(mini+maxi)/2;
-    if(mini==0&&maxi==0)break;
-    else{
-        k++;
-        ans.push_back(mid);
-        for(int i=0;i<n;i++)a[i]=abs(a[i]-mid);
-    }
-    if(k>40)break;
-   }
-   if(k<=40){
-    cout<<k<<endl;
-    for(int i=0;i<k;i++)cout<<ans[i]<<" ";
-    cout<<endl;
-   }
-   else cout<<-1<<endl;
-
+    cout<<"Maintaining the streak"
 }
 return 0;
 }
