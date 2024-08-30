278c5d6ecc60d6b3a11bca155e7d6031dac07eaf
AnCyNitinChaudhary <nitinchaudhary7290@gmail.com>
2024-08-30 19:02:10 +0530

    still grinding and will grind

--- 1.cpp
+++ 1.cpp
@@ -1,43 +1,73 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define int long long
 #define endl '\n'
-#define int_max 1e18
-#define int_min -1e18
-ll MOD =1e9+7;
-//Grinding for becoming Specialist
-//treat 0 as -1 and now problem reduced to count no. of subarray whose sum is equal to 0
-ll subarraySum(vector<ll>& nums) {
-        ll n=nums.size();
-        ll sum=0;
-        ll ans=0;
-        unordered_map<ll,ll>um;
-        um[0]=1;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-            ll right=n-i;
-            ll left=um[sum];
-            ans+=((left%MOD)*(right%MOD))%MOD;
-            ans%=MOD;
-            um[sum]+=(i+2);
-            um[sum]%=MOD;
-        }
-        return ans;
-}
-int main(){
-ios::sync_with_stdio(false);
-cin.tie(NULL);
-ll t;
-cin>>t;
-while(t--){
+ll M = 1e9+7;
+#define vi vector<int>
+#define vll vector<ll>
+#define vb vector<bool>
+#define vvi vector<vi>
+#define vpii vector<pii>
+#define fastIO ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);cout.precision(numeric_limits<double>::max_digits10);
+#define setbits(n)  __builtin_popcountll(n)
+#define bitsize(n) (63 - __builtin_clzll(n))
+#define lcm(a,b) (a/__gcd(a, b)*b)
+//From this question i learn that-> Yes it might take time to get it accepted by it will definitely accepted if you keep trying it. I have changed my way of implementation and finally make it implemented.
+void Nitin_Chaudhary(){
+    ll n,m,k;
+    cin>>n>>m>>k;
     string s;
     cin>>s;
-    vector<ll>nums;
-    for(auto i:s){
-        if(i=='0')nums.push_back(-1);
-        else nums.push_back(1);
+    s+="L";
+    bool flag=true;
+    ll curr=-1;
+    while(curr<n){
+        // cout<<curr<<endl;
+        ll end=curr;
+        for(int i=curr+1;i<=n;i++){
+            if(s[i]=='L'){
+                end=i;
+                break;
+            }
+        }
+        if(end-curr<=m){
+            curr=end;
+            continue;
+        }
+        else{
+            ll finalend=end;
+            while(end-curr>m){
+                if(s[end-1]=='W'){
+                    if(k>0){
+                        k--;
+                        end--;
+                    }
+                    else break;
+                }
+                else break;
+            }
+            if(end-curr<=m){
+                curr=finalend;
+            }
+            else break;
+        }
+
     }
-    cout<<subarraySum(nums)<<endl;
+    if(curr==n)cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+    
+    
+    
+
 }
-return 0;
+
+signed main(){
+    fastIO;
+    ll t;
+    cin>>t;
+    while(t--){
+       Nitin_Chaudhary();
+    }
+    return 0;
 }
